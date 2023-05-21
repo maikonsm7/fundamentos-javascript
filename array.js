94a38b816dd73116e33595015816882568aa6847
maikonmonteiro <maikonsm43@gmail.com>
2023-05-21 15:30:01 -0400

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+// Manipulando array
+
+const valores = [1, 2, 3, 4, 5]
+
+console.log(valores[2]) // retorna o valor da posição 2
+valores.push(6) // adiciona um valor no final do array
+console.log(valores)
+valores.pop() // remove o ultimo elemento do array
+console.log(valores)
