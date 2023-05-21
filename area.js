22d2ddd295a61ec8aa4f7d305923c34ed57fdd94
maikonmonteiro <maikonsm43@gmail.com>
2023-05-21 14:43:26 -0400

    first commit

--- area.js
+++ area.js
@@ -1,6 +1,6 @@
 // Calcular a área de uma circunferência -> Pi * raio * raio
 
 let raio = 3
-let resultado = Math.PI * raio * raio
+let resultado = Math.PI * Math.pow(raio, 2) // Math.pow -> exponenciação
 
 console.log("O valor da área é: " + resultado.toFixed(2) + "m2")
