f7a87be0e812896b252c6901fbcbefa59c559609
maikonmonteiro <maikonsm43@gmail.com>
2023-05-21 12:58:30 -0400

    add: fundamentos-javascript

---
+++ area.js
@@ -0,0 +1,6 @@
+// Calcular a área de uma circunferência -> Pi * raio * raio
+
+let raio = 3
+let resultado = Math.PI * raio * raio
+
+console.log("O valor da área é: " + resultado + "m2")
---
+++ dados.js
@@ -0,0 +1,8 @@
+// Tipos de dados
+
+// Todo número em javascript é number
+
+console.log(typeof 31) // number
+console.log(typeof 10.55) // number
+console.log(typeof "Teste") // string
+console.log(typeof true) // boolean
