591fce076d2057f0b670bcc4e038de2314d36e87
maikonmonteiro <maikonsm43@gmail.com>
2023-05-27 21:10:12 -0400

    first commit

---
+++ destructuring.js
@@ -0,0 +1,28 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Maikon',
+    idade: 25,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 123
+    }
+}
+
+// extrair atributos do objeto de forma simplificada
+const {nome, idade} = pessoa // usar chaves quando for objeto
+console.log(nome, idade)
+
+const {endereco: {logradouro, numero}} = pessoa
+console.log(logradouro)
+
+// escolhendo o nome da variavel
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+// Destructuring em array
+const [a] = [10]
+console.log(a);
+
+const [n1, ,n3, ,n5, n6 = 0] = [1, 2, 3, 4, 5] // ignora os elementos 2 e 4
+console.log(n1, n3, n5, n6);
